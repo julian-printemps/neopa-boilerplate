ea9631bde7c1aba75c6607b9debb4e8710176d20
Julian <julian.springaux@neopa.jp>
2017-09-15 15:59:02 +0900

    Adding jquery to dependency + rename no-js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,6 @@
-import './lib/reset';
+
+import './lib/no-js';
 
 // If you want to enable and choose which modules to include, and uncomment the line below
 // import './lib/foundation-explicit-pieces';
+// import $ from 'jquery';
