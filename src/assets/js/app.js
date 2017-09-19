e32a97c5d7821d22282da0bf6e394bbe885df3f1
Julian <julian.springaux@neopa.jp>
2017-09-19 18:22:14 +0900

    Adding CSS grid maker

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,3 +4,12 @@ import './lib/no-js';
 // If you want to enable and choose which modules to include, and uncomment the line below
 // import './lib/foundation-explicit-pieces';
 // import $ from 'jquery';
+
+
+// Menu
+let buttonMenu = document.getElementById('toggle_menu'),
+    navigation = document.getElementById('js_menu');
+
+buttonMenu.onclick = () => {
+  navigation.classList.toggle('is-active');
+};
